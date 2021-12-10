0622dcda6c0384dac9f46a1fb04659c6a6b7e9c6
Cadufc91 <fernandes.cadu@gmail.com>
2021-12-10 17:03:58 -0300

    arquivos parte 2 JS na Web formacao frontend Alura

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -2,15 +2,18 @@ const criarTarefa = (evento) => {
 
     evento.preventDefault()
 
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
     
-
-    const tarefa = document.querySelector('[data-task]')
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    
     const conteudo = `<p class="content">${valor}</p>`
     
     tarefa.innerHTML = conteudo
-    
+
+    lista.appendChild(tarefa)
     input.value = " "
 
 }
