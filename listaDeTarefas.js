14f27f50eea0aa544a53863061b88e8c49c5c4b8
Cadufc91 <fernandes.cadu@gmail.com>
2021-12-10 17:45:56 -0300

    Escondendo tarefas no JS

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,4 +1,5 @@
-const criarTarefa = (evento) => {
+( () => { 
+    const criarTarefa = (evento) => {
 
     evento.preventDefault()
 
@@ -8,11 +9,12 @@ const criarTarefa = (evento) => {
     
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-    
+
     const conteudo = `<p class="content">${valor}</p>`
     
     tarefa.innerHTML = conteudo
 
+    tarefa.appendChild(BotaoConclui())
     lista.appendChild(tarefa)
     input.value = " "
 
@@ -21,3 +23,22 @@ const criarTarefa = (evento) => {
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'concluir'
+    botaoConclui.addEventListener('click', concluirTarefa)
+
+    return botaoConclui
+}
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}
+}) ()
